ca40d5d340c2b757cd076fe8a2501cf9abe9ff90
FelipeFernandes777 <felipe524.fernades@gmail.com>
2023-08-23 20:18:12 -0300

    feat: Config Eslint and Prettier and absolute paths

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import bodyParser from 'body-parser';
 import express, { Application } from 'express';
+import { routes } from '@routes';
 
 const app: Application = express();
 
 app.use(bodyParser.json(), express.json());
 
+//Routes
+app.use(routes);
 
 export { app };
