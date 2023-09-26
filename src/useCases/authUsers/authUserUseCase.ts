ae2802eb51ef0df3bbee8176b6eebad1886a8755
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2023-09-26 19:21:40 -0300

    fix: login with email

--- src/useCases/authUsers/authUserUseCase.ts
+++ src/useCases/authUsers/authUserUseCase.ts
@@ -24,6 +24,7 @@ class AuthUserUseCase {
         const generateTokenProvider = new GenerateToken(userAlreadyExists.id);
 
         const token = await generateTokenProvider.generateToken();
+        
         return token;
     }
 
